1e8dde198fa1be4cc46ab0aa7cbe0b6be5e93be2
Divya Gupta <divygupta0319@gmail.com>
2021-06-15 18:03:48 +0530

    Added mapping to update an existing employee by PUT /employees

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,16 @@ public class EmployeeRESTController {
 		// this is to force a save of new item .. instead of update
 		
 		theEmployee.setId(0);
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	// add mapping  for PUT /employees - update existing employee
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		
 		employeeService.save(theEmployee);
 		
