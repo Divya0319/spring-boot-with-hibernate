86dba75d31b08064bc2d74874599e9987e0943d8
Divya Gupta <divygupta0319@gmail.com>
2021-06-15 17:06:47 +0530

    Added method definitions, for finding an employee by id, saving, and deleting an employee

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -23,18 +23,20 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	@Transactional
 	public List<Employee> findAll() {
-		return null;
+		return employeeDAO.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Employee findById(int theId) {
-		return null;
+		return employeeDAO.findById(theId);
 	}
 
 	@Override
 	@Transactional
 	public void save(Employee theEmployee) {
+		
+		employeeDAO.save(theEmployee);
 
 	}
 
@@ -42,6 +44,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Transactional
 	public void deleteById(int theId) {
 
+		employeeDAO.deleteById(theId);
 	}
 
 }
