1bfc1c4d02ae5e9f61a8375f0e70e18c5e60032e
Divya Gupta <divygupta0319@gmail.com>
2021-06-15 17:07:21 +0530

    Added mappings for getting an employee by id

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
@@ -4,29 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRESTController {
 	
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 	
 	// quick and dirty : inject employee dao
 	@Autowired
-	public EmployeeRESTController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	public EmployeeRESTController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	
 	// expose "/employees" and return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
-		return employeeDAO.findAll();
+		return employeeService.findAll();
+	}
+	
+	// add mapping for GET /employees/{employeeId}
+	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		return employeeService.findById(employeeId);
 	}
 
 }
