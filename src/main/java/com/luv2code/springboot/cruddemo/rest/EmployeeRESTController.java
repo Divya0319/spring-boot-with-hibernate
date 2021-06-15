944e6b4060ab15c2040263f386b036ff8eabff98
Divya Gupta <divygupta0319@gmail.com>
2021-06-15 18:15:29 +0530

    Added mapping to delete an employee by his id by DELETE /employees/{employeeId}

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRESTController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,6 +71,25 @@ public class EmployeeRESTController {
 		employeeService.save(theEmployee);
 		
 		return theEmployee;
+	}
+	
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		// throw exception if null
+		
+		if(tempEmployee == null) {
+			throw new RuntimeException("EMployee not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
+		
 	}
 
 }
