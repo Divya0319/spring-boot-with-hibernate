1cd095473d99cc2cdeb431fd8fd3f42ad9a38eac
Divya Gupta <divygupta0319@gmail.com>
2021-06-15 15:58:56 +0530

    Added @Service annotation to service class, added constructor injection for EntityManager

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,9 +2,22 @@ package com.luv2code.springboot.cruddemo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+		
+		employeeDAO = theEmployeeDAO;
+	}
 
 	@Override
 	public List<Employee> findAll() {
